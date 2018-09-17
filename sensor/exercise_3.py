15e391bfc5fdc25a2b5feba58221bad08be3b02c
Juan Jimeno <jimenojmm@gmail.com>
2018-09-17 03:17:38 -0400

    updated hands-on exercises

--- sensor/exercise_3.py
+++ sensor/exercise_3.py
@@ -1,5 +1,8 @@
 import time
 import bakebit
+import requests
+
+URL = 'http://localhost:8000/sensors'
  
 # Connect the BakeBit Light Sensor to analog port A0
 # SIG,NC,VCC,GND
@@ -18,11 +21,23 @@ time.sleep(1)
  
 # Reference voltage of ADC is 5v
 adc_ref = 5
- 
+
+def send_to_server(type, value):
+    try:
+        payload = {"sensors": type, "value": value}
+        print payload
+        r = requests.post(URL, data=payload)
+        print r.text
+    except:
+        print("Server Down")
+
+
 while True:
     try:
         # Read sensor value from potentiometer
         sensor_value = bakebit.analogRead(light_sensor)
+
+        send_to_server("lux", sensor_value)
  
         # Calculate voltage
         voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
@@ -34,7 +49,7 @@ while True:
             brightness = 255
         else:
             brightness = 0
-
+ 
         # Give PWM output to LED
         bakebit.analogWrite(led,brightness)
  
